860fe3d20358cd4197c700b6fd3d90f0593051cd
Freddie Stewart <info@freddiestewart.com>
2018-08-09 12:47:26 -0400

    Created staff_header.php and staff_footer.php in the shared directory.  The shared elements were then added to index.php using the include function.

--- public/staff/index.php
+++ public/staff/index.php
@@ -5,34 +5,10 @@
 	require_once('../../private/initialize.php'); 
 ?>
 
-<!doctype html>
+<?php include('../../private/shared/staff_header.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-    <!-- Using the Browser path and not the file path -->
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
-  </head>
+	<div id="content">
 
-  <body>
-
-  	<header>
-  		<h1>GBI Staff Area</h1>
-  	</header>
-
-  	<navigation>
-  		<ul>
-  			<li><a href="index.php">Menu</a></li>
-  		</ul>
-  	</navigation>
-
-  	<div id="content">
-
-  	</div>
-
-  	<footer>
-  		&copy <?php echo date('Y'); ?> Globe Bank
-  	</footer>
-  </body>
-</html>
+	</div>
+	
+<?php include('../../private/shared/staff_footer.php'); ?>	
