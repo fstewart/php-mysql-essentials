5233235da12644e68052b47ce5c6ea4f8d04e930
Freddie Stewart <info@freddiestewart.com>
2018-08-13 11:34:19 -0400

    Updated the functions and Readme.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 PHP with MySQL Essentials Training with Kevin Skoglund
 
+This project includes:
+- MySQL connections using PHP 7
+- How to Blueprint an application
+- Accessing shared resources
+- Basic links and urls
+- Using URL parameters
+- Setting defaults for url parameters
+- Encoding URL parameters
+- Encoding for HTML - Security measure!
+
 Blueprint
 
 Public Area
--- private/functions.php
+++ private/functions.php
@@ -1,4 +1,6 @@
 <?php
+
+	// Reference the correct url based on the location of the current php file in relation to the root of the website. 
 	function url_for($script_path) {
 		// add the leading '/' if not present
 		if($script_path[0] != '/') {
@@ -6,4 +8,38 @@
 		}
 		return WWW_ROOT . $script_path;
 	}
+
+	// Encode a string to be used in a query part of a url.  It is conveneint when passing variables to the next page. 
+	function u($string="") {
+		return urlencode($string);
+	}
+
+	// Encodes a given string according to RFC 3986.
+	function raw_u($string="") {
+		return rawurlencode($string);
+	}
+
+	// Converts special characters to HTML.  This prevents html characters from being dynamically added.  This is a security requirement and protects against vulnerability.
+	function h($string="") {
+		return htmlspecialchars($string);
+	}
+
+	// Displays server error 404 Not Found.
+	function error_404() {
+		header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+		exit();
+	}
+
+	// Displays server error 500 Internal Server Error.
+	function error_500() {
+		header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+		exit();
+	}
+
+	// Redirects to a provided address using the header parameter.d
+	function redirect_to($location) {
+		header("Location: " . $location);
+		exit;
+	}
+
 ?>
